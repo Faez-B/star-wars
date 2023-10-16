022d012208ce807fe87982ca10024dd3f930cf22
Faez-B <faezbhatti@gmail.com>
2023-10-16 17:26:30 +0200

    update : page index Affichage des joueurs dans la BDD

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,17 +2,25 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Joueur;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MainController extends AbstractController
 {
+    public function __construct(
+        private EntityManagerInterface $em,
+    ) {}
+
     #[Route('/', name: 'main')]
     public function index(): Response
     {
+        $joueurs = $this->em->getRepository(Joueur::class)->findAll();
+
         return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
+            'joueurs' => $joueurs,
         ]);
     }
 }
