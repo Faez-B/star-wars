9823d0a78036172389ae4444b14d13085e450abc
Faez-B <faezbhatti@gmail.com>
2023-10-16 19:14:07 +0200

    update : API route create player using ally code

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Joueur;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,7 +13,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 #[Route('/api')]
 class ApiController extends AbstractController
 {
-    #[Route('/{allyCode}/guild', name: 'guild_infos')]
+    public function __construct(
+        private EntityManagerInterface $em,
+    ) {}
+
+    #[Route('/{allyCode}/guild', name: 'guild_infos', methods: ['GET'])]
     public function getGuildInfos(int $allyCode): JsonResponse
     {
         $client = HttpClient::create();
@@ -24,13 +30,33 @@ class ApiController extends AbstractController
         $response = $client->request('GET', 'https://swgoh.gg/api/guild-profile/' . $guildID);
         $data = ($response->toArray())['data'];
 
-        // return $this->json($data);
-
         return $this->json([
             'id' => $data['guild_id'],
             'nom' => $data['name'],
             'puisGalactique' => $data['galactic_power'],
             'nbJoueurs' => count($data["members"]),
         ]);
+    }
+
+    #[Route('/{allyCode}/create', name: 'create_player')]
+    public function createPlayer(int $allyCode): Response
+    {
+        $client = HttpClient::create();
+        $response = $client->request('GET', 'https://swgoh.gg/api/player/' . $allyCode);
+        $data = ($response->toArray())['data'];
+
+        $joueur = new Joueur();
+        $joueur->setAllyCode($allyCode);
+        $joueur->setPseudo($data['name']);
+        $joueur->setTitre($data['title']);
+        $joueur->setNiveau($data['level']);
+        $joueur->setPuisGalactiqueTotale($data['galactic_power']);
+        $joueur->setPuisGalactiqueHeros($data['character_galactic_power']);
+        $joueur->setPuisGalactiqueVaisseaux($data['ship_galactic_power']);
+        
+        $this->em->persist($joueur);
+        $this->em->flush();
+
+        return new Response('Joueur créé', Response::HTTP_OK);
     }
 }
