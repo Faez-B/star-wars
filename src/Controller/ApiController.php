86687ec67fc7479bdaa7f8758734e4dde9695176
Faez-B <faezbhatti@gmail.com>
2023-10-16 18:30:04 +0200

    Début de l'API : récupérer les infos de la guilde

---
+++ src/Controller/ApiController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+#[Route('/api')]
+class ApiController extends AbstractController
+{
+    #[Route('/{allyCode}/guild', name: 'guild_infos')]
+    public function getGuildInfos(int $allyCode): JsonResponse
+    {
+        $client = HttpClient::create();
+        $response = $client->request('GET', 'https://swgoh.gg/api/player/'.$allyCode.'/');
+
+        $data = $response->toArray();
+
+        return $this->json($data);
+    }
+}
