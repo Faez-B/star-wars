0bece279be7e1a298e78be01d9935aef38208347
Faez-B <faezbhatti@gmail.com>
2023-10-18 21:44:52 +0200

    corr Héros

---
+++ migrations/Version20231018194243.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20231018194243 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE heros CHANGE degat_critique degat_critique DOUBLE PRECISION NOT NULL, CHANGE vol_vie vol_vie DOUBLE PRECISION NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE heros CHANGE degat_critique degat_critique INT NOT NULL, CHANGE vol_vie vol_vie INT NOT NULL');
+    }
+}
